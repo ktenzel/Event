79609465e527846903cc3c65229d61b75bacabe1
kyle tenzeldam <ktenzel@gmail.com>
2018-01-05 15:44:38 -0800

    test for single input and multi input pass

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,47 +1,22 @@
 package models;
 
 public class Event {
-    public String numberOfGuest;
+    private int guestCount;
 
-    public static String calculateEvent(String numberOfGuest){
+    public Event(int guest) {
+        guestCount = guest;
+    }
+
+    public int getGuestCount() {
+        return guestCount;
+    }
+
+
+    public static String calculateEvent(String numberOfGuest) {
         int guest = Integer.parseInt(numberOfGuest);
         Integer plate = 30;
         Integer cost = guest * plate;
         String partyCost = Integer.toString(cost);
         return partyCost;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public Integer numberGuest;
-    public String foodChoice;
-    public String drinkChoice;
-    public String entertainmentChoice;
-    public Integer initialGuestCost;
-    public Event(int guest, String food, String drink, String entertainment){
-        numberGuest = guest;
-        foodChoice = food;
-        drinkChoice = drink;
-        entertainmentChoice = entertainment;
-    }
-    public int getGuestCount() {
-        return numberGuest;
-    }
-    public Integer getPricePerGuest(Integer initialGuestCost){
-        initialGuestCost = numberGuest * 30;
-        return initialGuestCost;
     }
 }
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class EventTest {
     @Test
     public void runEvent_initiatesEvent() {
-        Event testEvent = new Event();
+        Event testEvent = new Event(1);
         assertEquals(true,testEvent instanceof Event);
     }
     @Test
